2f31328bce132615d96ffe3669c9e8c118e2caa2
Natthanon Manop <natthanon.manop@gmail.com>
2020-11-17 22:20:18 +0700

    Add Content-Security-Policy header but still warn in ZAP

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,8 +2,8 @@ module.exports = {
   devServer: {
     headers: {
       "X-Frame-Options": "sameorigin",
+      "Content-Security-Policy":
+        "script-src 'self' localhost:3000; frame-ancestors localhost:3000;",
     },
   },
 };
-
-// "Content-Security-Policy": "default-src 'self';base-uri 'self';object-src 'self';script-src 'self';style-src 'self';frame-ancestors 'self'",
